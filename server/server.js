19a7dcfd394ab54fdf429c8f99f74ec818528f27
Juan Valentin Alducin  Vazquez <juan.alducin@stkprojects.com>
2021-01-15 22:45:07 -0600

    RestServer-usuario

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
-const express = require('express');
 require('./config/config');
+
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -11,39 +13,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('getUsuario')
-});
 
-app.post('/usuario', function(req, res) {
+app.use(require('./routes/usuario'));
 
-    let body = req.body;
+//useNewUrlParser: true, useCreateIndex:
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre ese necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
+mongoose.connect(process.env.URLDB, (err, res) => {
 
+    if (err) throw err;
 
-});
+    console.log('Base de datos ONLINE');
 
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('deleteUsuario')
 });
 
 app.listen(process.env.PORT, () => {
