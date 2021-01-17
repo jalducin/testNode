f392595627bba0deae1c53579ecf937bf591a15f
Juan Valentin Alducin  Vazquez <juan.alducin@stkprojects.com>
2021-01-16 18:03:24 -0600

    Login normal

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,9 +1,13 @@
 const express = require('express');
+
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
-const app = express();
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
+
+const app = express();
+
 
 /**
  * strider
@@ -13,7 +17,7 @@ const Usuario = require('../models/usuario');
  * mongodb+srv://strider:<password>@cluster0.a2lte.mongodb.net/test
  */
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
 
 
     let desde = req.query.desde || 0;
@@ -22,7 +26,6 @@ app.get('/usuario', function(req, res) {
     let limite = req.query.limite || 5;
     limite = Number(limite);
 
-    //Limitar campos a mostrar
     Usuario.find({ estado: true }, 'nombre email role estado google img')
         .skip(desde)
         .limit(limite)
@@ -48,9 +51,10 @@ app.get('/usuario', function(req, res) {
 
         });
 
+
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let body = req.body;
 
@@ -71,8 +75,6 @@ app.post('/usuario', function(req, res) {
             });
         }
 
-        usuarioDB.password = null;
-
         res.json({
             ok: true,
             usuario: usuarioDB
@@ -81,9 +83,11 @@ app.post('/usuario', function(req, res) {
 
     });
 
+
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
+
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
@@ -96,15 +100,18 @@ app.put('/usuario/:id', function(req, res) {
             });
         }
 
+
+
         res.json({
             ok: true,
             usuario: usuarioDB
         });
 
     })
+
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
 
     let id = req.params.id;
